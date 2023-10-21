b74b166d87e932bfdf2f85c52d6109bc88884013
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-10-21 11:06:43 +0300

    refactor(types): combine types in company facts schema

--- libs/types/src/edgar-api/company-facts.ts
+++ libs/types/src/edgar-api/company-facts.ts
@@ -1,33 +1,52 @@
 import { z } from 'zod';
 
-export const FactSchema = z.record(
-  z.object({
-    label: z.string(),
-    description: z.string(),
-    units: z.record(
-      z.array(
-        z.object({
-          start: z.string().optional(),
-          end: z.string(),
-          val: z.number(),
-          accn: z.string(),
-          fy: z.number(),
-          fp: z.string(),
-          form: z.string(),
-          filed: z.string(),
-          frame: z.string().optional(),
-        }),
-      ),
-    ),
-  }),
-);
-
-export type Fact = z.infer<typeof FactSchema>;
-
 export const CompanyFactsSchema = z.object({
   cik: z.number(),
   entityName: z.string(),
-  facts: z.object({ dei: FactSchema, 'us-gaap': FactSchema }),
+  facts: z.object({
+    dei: z.record(
+      z.object({
+        label: z.string(),
+        description: z.string(),
+        units: z.record(
+          z.array(
+            z.object({
+              start: z.string().optional(),
+              end: z.string(),
+              val: z.number(),
+              accn: z.string(),
+              fy: z.number(),
+              fp: z.string(),
+              form: z.string(),
+              filed: z.string(),
+              frame: z.string().optional(),
+            }),
+          ),
+        ),
+      }),
+    ),
+    'us-gaap': z.record(
+      z.object({
+        label: z.string(),
+        description: z.string(),
+        units: z.record(
+          z.array(
+            z.object({
+              start: z.string().optional(),
+              end: z.string(),
+              val: z.number(),
+              accn: z.string(),
+              fy: z.number(),
+              fp: z.string(),
+              form: z.string(),
+              filed: z.string(),
+              frame: z.string().optional(),
+            }),
+          ),
+        ),
+      }),
+    ),
+  }),
 });
 
 export type CompanyFacts = z.infer<typeof CompanyFactsSchema>;
