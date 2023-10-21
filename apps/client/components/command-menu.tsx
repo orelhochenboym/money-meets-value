41785135badee3daf2e54ad6b3de76515bf549cf
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-10-21 12:26:44 +0300

    feat(components): create command menu with dialog and command components

---
+++ apps/client/components/command-menu.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import * as React from 'react';
+import { useRouter } from 'next/navigation';
+import { DialogProps } from '@radix-ui/react-alert-dialog';
+import { cn } from '../lib/utils';
+import { Button } from './ui/button';
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from './ui/command';
+
+export function CommandMenu({ ...props }: DialogProps) {
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.code === 'Slash') {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener('keydown', down);
+    return () => document.removeEventListener('keydown', down);
+  }, []);
+
+  const runCommand = React.useCallback((command: () => unknown) => {
+    setOpen(false);
+    command();
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className={cn(
+          'text-muted-foreground relative w-full justify-start text-sm sm:pr-12 md:w-40 lg:w-64',
+        )}
+        onClick={() => setOpen(true)}
+        {...props}
+      >
+        <span className="hidden lg:inline-flex">Search documentation...</span>
+        <span className="inline-flex lg:hidden">Search...</span>
+        <kbd className="bg-muted pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          {/* <CommandGroup heading="Links">{}</CommandGroup> */}
+          {}
+          <CommandSeparator />
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
