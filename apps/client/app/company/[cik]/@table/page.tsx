ae23d3b1a83cc45ed71eedc04377dd7fbe7c6a12
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-10-22 18:18:32 +0300

    feat(table): create basic table that shows data from edgar api

--- apps/client/app/company/[cik]/@table/page.tsx
+++ apps/client/app/company/[cik]/@table/page.tsx
@@ -1,4 +1,12 @@
 import { CompanyFacts, CompanyFactsSchema } from '@money-meets-value/types';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '../../../../components/ui/table';
 import { fromZodError } from 'zod-validation-error';
 
 const getCompanyFacts = async (cik: string) => {
@@ -19,6 +27,53 @@ const getCompanyFacts = async (cik: string) => {
 
 export default async function Index({ params }: { params: { cik: string } }) {
   const companyfacts = await getCompanyFacts(params.cik);
+  const tenLastYears = [...Array(10).keys()].map(
+    (_, i) => new Date().getFullYear() - 1 - i,
+  );
 
-  return <div className="h-full w-full">Table</div>;
+  return (
+    <div className="h-full w-full">
+      <Table className="h-full w-full border">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Metric</TableHead>
+            {tenLastYears.map((year) => (
+              <TableHead key={year}>{year}</TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Object.keys(companyfacts.facts.dei).map((key, i) => (
+            <TableRow key={i}>
+              <TableCell>{companyfacts.facts.dei[key].label ?? key}</TableCell>
+              {tenLastYears.map((year) => (
+                <TableCell key={year}>
+                  {Object.keys(companyfacts.facts.dei[key].units).map(
+                    (unitKey) => {
+                      return (
+                        <TableCell key={unitKey}>
+                          {parseFloat(
+                            (
+                              (companyfacts.facts.dei[key].units[unitKey].find(
+                                (fact) => {
+                                  return (
+                                    fact.form.includes('K') &&
+                                    fact.frame?.includes(year.toString())
+                                  );
+                                },
+                              )?.val ?? 0) / 1000000
+                            ).toFixed(2),
+                          ).toLocaleString() ?? 'N/A'}
+                        </TableCell>
+                      );
+                    },
+                  )}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
