bc601647a43759dd9dcb0069590dd8bef9260a7b
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-11-04 20:45:12 +0200

    feat(header): use new search button and modal

--- apps/client/components/header.tsx
+++ apps/client/components/header.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Logo } from './logo';
 import { Navbar } from './navbar';
 import { SearchButton } from './search/search-button';
+import SearchModal from './search/search-modal';
 
 export const Header: React.FC = async () => {
+  const modalId = 'search-modal';
   return (
     <div className="border-accent flex items-center justify-between border-b-2 p-3">
       <Logo />
       <Navbar />
-      <SearchButton modalId="search-modal" />
+      <SearchButton modalId={modalId} />
+      <SearchModal id={modalId} />
     </div>
   );
 };
