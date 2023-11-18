c21abbbaca2e9156249668042cc8df2c8c88e35d
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-11-18 15:22:06 +0200

    refactor(company-logo): change sizing of logo

--- apps/client/app/company/[cik]/@info/components/company-logo.tsx
+++ apps/client/app/company/[cik]/@info/components/company-logo.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export const CompanyLogo: React.FC<Props> = ({ companyInfo }) => {
   return (
-    <div className="aspect-square h-full">
+    <div className="aspect-square h-1/4 w-1/4">
       <Avatar className="inline-flex h-full w-full max-w-full select-none items-center justify-center overflow-hidden border align-middle ">
         <AvatarImage className="object-contain" />
         <AvatarFallback>{companyInfo.symbol}</AvatarFallback>
