9c31ce1914ab95d346c30a16b697a8582054dfe7
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-11-18 15:45:18 +0200

    fix(breadcrumbs): improve performance by using map instead of map and flatMap

--- apps/client/app/company/[cik]/@info/components/breadcrumbs.tsx
+++ apps/client/app/company/[cik]/@info/components/breadcrumbs.tsx
@@ -3,19 +3,20 @@ type Props = { items: string[] };
 export const Breadcrumbs: React.FC<Props> = ({ items }) => {
   return (
     <div className="text-muted-foreground flex h-fit w-fit items-center gap-2 whitespace-nowrap">
-      {items
-        .map((item, i) => <span key={i}>{item}</span>)
-        .flatMap((item, i, array) =>
-          array.length - 1 !== i
-            ? [
-                item,
-                <div
-                  key={i}
-                  className="bg-muted-foreground h-1.5 w-1.5 rounded-full"
-                />,
-              ]
-            : item,
-        )}
+      {items.map((item, i, array) => {
+        const itemJsx = <div key={`${i}${item}`}>{item}</div>;
+
+        if (array.length - 1 !== i) {
+          return [
+            itemJsx,
+            <div
+              key={i}
+              className="bg-muted-foreground h-1.5 w-1.5 rounded-full"
+            />,
+          ];
+        }
+        return itemJsx;
+      })}
     </div>
   );
 };
