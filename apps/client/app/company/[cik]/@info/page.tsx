d64743a5efece697e891041652f3dadaa8499bd1
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-11-22 10:20:17 +0200

    refactor(info): use nyf instead of server

--- apps/client/app/company/[cik]/@info/page.tsx
+++ apps/client/app/company/[cik]/@info/page.tsx
@@ -1,38 +1,37 @@
+import { getCompanies } from '@money-meets-value/utils';
 import Link from 'next/link';
-import { Quote } from 'yahoo-finance2/dist/esm/src/modules/quote';
-import { SearchResult } from 'yahoo-finance2/dist/esm/src/modules/search';
+import yahooFinance from 'yahoo-finance2';
 import { Breadcrumbs } from './components/breadcrumbs';
 import { CompanyInfo } from './components/company-info';
 import { CompanyLogo } from './components/company-logo';
 import { CompanyPrice } from './components/company-price';
 import { Metrics } from './components/metrics';
 
-const getCompanyQuote = async (cik: string) => {
-  const companyInfo: Quote = await fetch(
-    `http://localhost:3000/api/quote/${cik}`,
-    {
-      cache: 'no-cache',
-    },
-  ).then((res) => res.json());
-
-  return companyInfo;
-};
-const getCompanySearch = async (symbol: string) => {
-  const companySearch: SearchResult = await fetch(
-    `http://localhost:3000/api/search/${symbol}`,
-    {
-      cache: 'no-cache',
-    },
-  ).then((res) => res.json());
-
-  return companySearch;
-};
-
 export default async function Index({ params }: { params: { cik: string } }) {
-  const companyInfo = await getCompanyQuote(params.cik);
-  const companySearch = await getCompanySearch(companyInfo.symbol);
-  const foundCompanySearch = companySearch.quotes.find(
-    (quote) => companyInfo.symbol === quote.symbol,
+  const companies = await getCompanies();
+  const relatedCompany = companies.find((company) => {
+    for (const key in company) {
+      if (company[key] !== Number(params.cik)) {
+        continue;
+      }
+      return true;
+    }
+  });
+
+  if (!relatedCompany) {
+    throw new Error('No company found');
+  }
+
+  const ticker =
+    relatedCompany[
+      Object.keys(relatedCompany).find((key) => key === 'ticker') ?? ''
+    ]?.toString() ?? '';
+
+  const quote = await yahooFinance.quote(ticker);
+  const search = await yahooFinance.search(quote.symbol);
+
+  const foundCompanySearch = search.quotes.find(
+    (searchQuote) => quote.symbol === searchQuote.symbol,
   );
 
   return (
@@ -47,12 +46,12 @@ export default async function Index({ params }: { params: { cik: string } }) {
       <div className="flex h-fit w-full justify-between">
         <div className="flex h-fit w-fit flex-col">
           <div className="flex h-fit w-full items-center justify-start gap-2">
-            <CompanyLogo companyInfo={companyInfo} />
-            <CompanyInfo companyInfo={companyInfo} />
+            <CompanyLogo companyInfo={quote} />
+            <CompanyInfo companyInfo={quote} />
           </div>
-          <CompanyPrice companyInfo={companyInfo} />
+          <CompanyPrice companyInfo={quote} />
         </div>
-        <Metrics companyInfo={companyInfo} />
+        <Metrics companyInfo={quote} />
       </div>
       <div className="flex h-fit w-fit gap-4">
         <Link href="#">Summary</Link>
