a3f6813f94889d386b4eed5fbbf5200cb760cb41
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-11-24 18:01:27 +0200

    feat(summary): show chart card when summary is selected

---
+++ apps/client/app/company/[cik]/page.tsx
@@ -0,0 +1,35 @@
+import { getCompanies } from '@money-meets-value/utils';
+import yahooFinance from 'yahoo-finance2';
+import { Chart } from '../../../components/chart';
+import { Card } from '../../../components/ui/card';
+
+export default async function Index({ params }: { params: { cik: string } }) {
+  const companies = await getCompanies();
+  const relatedCompany = companies.find((company) => {
+    for (const key in company) {
+      if (company[key] !== Number(params.cik)) {
+        continue;
+      }
+      return true;
+    }
+  });
+
+  if (!relatedCompany) {
+    throw new Error('No company found');
+  }
+
+  const ticker =
+    relatedCompany[
+      Object.keys(relatedCompany).find((key) => key === 'ticker') ?? ''
+    ]?.toString() ?? '';
+
+  const companyChart = await yahooFinance.chart(ticker, {
+    period1: 1667236413,
+  });
+
+  return (
+    <Card className="h-full w-full overflow-visible p-4">
+      <Chart chart={companyChart}></Chart>
+    </Card>
+  );
+}
---
+++ apps/client/components/chart.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { getMonth, isEqual, min } from 'date-fns';
+import getYear from 'date-fns/getYear';
+import {
+  Area,
+  AreaChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { ChartResultArray } from 'yahoo-finance2/dist/esm/src/modules/chart';
+
+type Props = { chart: ChartResultArray };
+
+export const Chart: React.FC<Props> = ({ chart }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <AreaChart data={chart.quotes}>
+        <defs>
+          <linearGradient id="price" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#2563EB" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#2563EB" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis
+          dataKey="date"
+          tickFormatter={(value) => {
+            const date = new Date(value);
+            const year = getYear(date);
+            const month = date.toLocaleString('default', { month: 'short' });
+
+            return `${month} ${year}`;
+          }}
+          ticks={chart.quotes
+            .filter((quote) => {
+              const month = getMonth(quote.date);
+              const year = getYear(quote.date);
+
+              const restOfDaysOfMonth = chart.quotes
+                .filter(
+                  (quote) =>
+                    getMonth(quote.date) === month &&
+                    getYear(quote.date) === year,
+                )
+                .map((quote) => quote.date);
+
+              const minTradingDateOfMonth = min(restOfDaysOfMonth);
+
+              return (
+                getMonth(quote.date) % 2 === 0 &&
+                isEqual(quote.date, minTradingDateOfMonth)
+              );
+            })
+            .map((quote) => quote.date.getTime())}
+        />
+        <YAxis domain={['auto', 'auto']} />
+        <Tooltip
+          formatter={(value, _name, _props) => {
+            return [`${Number(value).toFixed(2)}`, 'Price'];
+          }}
+          itemStyle={{ color: 'hsl(var(--foreground))' }}
+          labelFormatter={(label) => {
+            return new Date(label).toLocaleDateString();
+          }}
+          labelStyle={{ color: 'hsl(var(--muted-foreground))' }}
+        />
+        <Area
+          type="monotone"
+          dataKey="close"
+          stroke="#2563EB"
+          fill="url(#price)"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
