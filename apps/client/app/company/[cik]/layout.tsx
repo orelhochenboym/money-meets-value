e62b152ddd31d3b8c447447f633de2208c373f48
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-11-25 16:43:01 +0200

    refactor: remove redundant parallel routes

--- apps/client/app/company/[cik]/layout.tsx
+++ apps/client/app/company/[cik]/layout.tsx
@@ -1,13 +1,10 @@
 export default function Layout({
-  table,
   info,
-  chart,
   children,
 }: {
   children: React.ReactNode;
   table: React.ReactNode;
   info: React.ReactNode;
-  chart: React.ReactNode;
 }) {
   return (
     <div className="flex h-full w-full flex-col items-center justify-start overflow-visible">
