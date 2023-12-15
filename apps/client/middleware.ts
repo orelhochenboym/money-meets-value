4c6e6f0be1d3fc23a96cde89b55767e42ae21301
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-12-15 15:20:35 +0200

    feat(auth): handle flow when user exists and needs sign in

--- apps/client/middleware.ts
+++ apps/client/middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: ['/', '/sso-callback'],
   afterAuth(auth, req, evt) {
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
