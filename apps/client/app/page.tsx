23dd18bb83f72bae2ef812ff057f1a493825d6ed
Orel Moshe Hochenboym <orelhochenboym@gmail.com>
2023-12-15 15:35:32 +0200

    feat(home): if user doenst exist in our db insert him

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,8 +1,21 @@
 import { auth } from '@clerk/nextjs';
+import { eq } from 'drizzle-orm';
 import { Toast } from '../components/toast/toast';
+import { db } from '../db/db';
+import { users } from '../db/schema/users';
 
 export default async function Index() {
   const { userId } = auth();
+
+  if (userId) {
+    const user = (
+      await db.select().from(users).where(eq(users.clerkId, userId))
+    ).find((user) => user.clerkId === userId);
+
+    if (!user) {
+      await db.insert(users).values({ clerkId: userId });
+    }
+  }
 
   return (
     <div className="h-full w-full">
